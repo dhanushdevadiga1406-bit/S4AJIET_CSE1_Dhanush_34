bf8d533b31fe28df758f6122a754b36d824e921d
dhanushdevadiga1406-bit <dhanushdevadiga1406@gmail.com>
2026-05-04 12:08:57 +0530

    Correct swap logic in swapwithalphabeticoperand.java
    Fix the swap logic to correctly swap values of a and b.

---
+++ swapwithalphabeticoperand.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int a=2;
+		int b=3;
+		int temp=a;
+		a=b;
+		b=a;
+    temp=b;
+		System.out.println("a ="+a);
+		System.out.println("b ="+b);
+	}
+}
+		
