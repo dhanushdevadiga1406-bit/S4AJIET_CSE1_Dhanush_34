2cead7b6c7bd546fd8419dccee86f4cf3e42c0fe
dhanushdevadiga1406-bit <dhanushdevadiga1406@gmail.com>
2026-05-06 14:13:16 +0530

    Implement character frequency counter in HashMap

---
+++ HashMap2.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+public class Main
+{
+	public static void main(String[] args){
+	    String s = "college";
+	    HashMap<Character,Integer>k=new HashMap<>();
+	    for(char c : s.toCharArray()){
+	        if(k.containsKey(c)){
+	            k.put(c,k.get(c)+1);
+	        }
+	        else{
+	            k.put(c,1);
+	            }
+	            }System.out.println(k);
+	}
+}
+	   
